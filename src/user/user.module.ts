81e99f78e68ff9c6d3b599b24df84e03790b9564
hoon2-kim <gooaba1204@gmail.com>
2026-01-06 02:27:45 +0900

    feature:get-user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,15 @@ import { CreateUserUseCase } from './application/use-cases/create-user.use-case'
 import { USER_REPOSITORY } from './application/ports/user.repository.port';
 import { InMemoryUserRepository } from './infrastructure/adapters/in-memory-user.repository';
 import { UserController } from './presentation/user.controller';
+import { GetUserUseCase } from './application/use-cases/get-user.use-case';
+import { ListUsesrUseCase } from './application/use-cases/list-users.use-case';
 
 @Module({
   controllers: [UserController],
   providers: [
     CreateUserUseCase,
+    GetUserUseCase,
+    ListUsesrUseCase,
     {
       provide: USER_REPOSITORY,
       useClass: InMemoryUserRepository,
